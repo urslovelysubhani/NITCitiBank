98893715a9bd64e9e6e7514d7fcd7bb6bb5acb15
TL <TL@gmail.com>
2023-08-05 16:20:35 +0530

    commited by TL

--- DemoProj1/src/com/nt/sbeans/Utility.java
+++ DemoProj1/src/com/nt/sbeans/Utility.java
@@ -8,8 +8,8 @@ public class Utility {
 	return new Date();
 }
 	public void m1() {
-		System.out.println("Utility.m1()method:1234");
-        System.out.println("abc123...");
+		System.out.println("Utility.m1()method");
+       
 	}
 
 }
