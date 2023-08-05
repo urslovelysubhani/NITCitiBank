48ee33980f3c1723091f9ed3b77dcdef6f56d1b5
TL <TL@gmail.com>
2023-08-05 14:55:49 +0530

    DEV1 modified the code

--- DemoProj1/src/com/nt/sbeans/Utility.java
+++ DemoProj1/src/com/nt/sbeans/Utility.java
@@ -7,13 +7,9 @@ public class Utility {
 	public Date getSysDate(){
 	return new Date();
 }
-
-
-	
-
 	public void m1() {
 		System.out.println("Utility.m1()method:1234");
-
+        System.out.println("abc123...");
 	}
 
 }
