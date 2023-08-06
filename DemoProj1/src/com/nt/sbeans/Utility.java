2dda94c335e831cd09e074bf3bb04d0362f0f5c1
TL <TL@gmail.com>
2023-08-06 16:16:40 +0530

    commited by dev1

--- DemoProj1/src/com/nt/sbeans/Utility.java
+++ DemoProj1/src/com/nt/sbeans/Utility.java
@@ -15,7 +15,15 @@ public class Utility {
 	public void m2() {
 		System.out.println(" m2()");
 	}
+
 	public void m4() {
 		System.out.println(" m4()");
 	}
+
+	public void m3() {
+	System.out.println(" m3()");	
+	}
+	
+
+
 }
